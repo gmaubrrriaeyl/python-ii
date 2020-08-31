7429025ce000c9cbf392330a0578f00a8634f71b
gmaubrrriaeyl <43591801+gmaubrrriaeyl@users.noreply.github.com>
2020-08-31 13:43:14 -0400

    Update MurrayClass.py

--- MurrayClass.py
+++ MurrayClass.py
@@ -12,25 +12,24 @@ Must have the followin functionality
 """
 
 class Contact:
+    
     counter = 0
     roster = []
-    
     def getCount():
         return Contact.counter
     
-    def __init__(self, last, first): #Constructor
+    def __init__(self, first, last): #Constructor
         
         self.first = first.title()
         self.last = last.title()
         self.full = first.title() + ' ' + last.title()
         Contact.counter += 1
-        self.ID = last[:3].lower() + str(Contact.counter)
-        
+        self.ID = last + str(Contact.counter)
         if self.full not in Contact.roster:
-            ID = str(self.last[:3]) + str(Contact.counter)
+            empid = str(self.last[:3]) + str(Contact.counter)
             Contact.roster.append(self.full)
         else:
-            print("Contact already in roster.")
+            print("Contact already in roster")
 
     
     def getRoster(): #Accessors
@@ -39,34 +38,26 @@ class Contact:
         return self.full
 
 
-gabe = Contact('murray', 'gabe') #Few defaults for the roster
-prof = Contact('prather', 'mark')
+        
 
+
+gabe = Contact('gabe', 'murray') #Few defaults for the roster
+prof = Contact('mark', 'prather')
 
 def main():
     print("Program options")
-    choices = str("1. Display all contact \n2. Create new contact\n3. Exit")
-
-    
-    while True:
-        print(choices)
-        option = int(input("Enter 1,2, or 3: "))
-        if not (isinstance(option, int) and (option<=3 and option>=1)):
-            print("Not a valid option.\n" + choices)
-        if option == 1:
-            print(str(Contact.getRoster()) + '\n')
-            continue
-        elif option == 2:
-            x = input("Enter contact in the following format: Doe, John" + '\n')
-            x = x.split(',')
-            x = [x.strip() for x in x]
-            if x not in Contact.roster:
-                x = Contact(x[0], x[1])
-                print(str(Contact.roster[-1] + " has been added to your contacts. Their contact id is " + x.ID + ". \n\n"))
-            
-            continue
-        elif option == 3:
-            return print("Goodbye World!")
-            
-if __name__ == "main":
-    main()
+    print("1. Display all contact")
+    print("2. Create new contact")
+    print("3. Exit")
+    option = int(input("Enter 1,2, or 3: ")
+    #if (isinstance(option, int) and (option<=3 and option>=1)):
+    #    print("Not a valid option. Exiting..")
+    if option == 1:
+        return Contact.getRoster()
+    elif option == 2:
+        x = input("Enter contact in the following format: Doe, John" + '\n')
+        x = x.split(',')
+        x = [x.strip() for x in x]
+        x = Contact(x[1], x[0])
+    elif option == 3:
+        print("Goodbye! Thanks for using my program :)")
